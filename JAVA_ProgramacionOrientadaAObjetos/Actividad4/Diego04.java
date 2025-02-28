2022d0bdb9800d3493deb0a95b4335e8260e4d67
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-02-27 22:46:52 -0600

    Add Colores class for color management and enhance Diego04 with text formatting methods

--- JAVA_ProgramacionOrientadaAObjetos/Actividad4/Diego04.java
+++ JAVA_ProgramacionOrientadaAObjetos/Actividad4/Diego04.java
@@ -14,17 +14,22 @@ public class Diego04 {
         NuevoVideojuego("Among Us", contador++);
         int opcion = 0;
 
+        LimpiarPantalla();
+
         while (true) {
             opcion = Menu();
             if (opcion == 5) break;
 
+            System.out.println();
+
             int id = 0;
             switch (opcion) {
                 case 1:
+                    TiposDeTexto.H2("Agregar videojuego", 4);
                     System.out.print("Nombre del videojuego > ");
                     String nombre = sc.nextLine();
                     NuevoVideojuego(nombre, contador++);
-                    System.out.println("Videojuego agregado: " + nombre);
+                    System.out.println("Videojuego agregado: " + nombre + " con ID " + (contador - 1));
                     break;
 
                 case 2:
@@ -32,17 +37,19 @@ public class Diego04 {
                     break;
                 
                 case 3:
+                    TiposDeTexto.H2("Actualizar nombre de videojuego", 8);
                     ListaElementos(videojuegos);
                     System.out.print("ID del videojuego a actualizar > ");
                     id = sc.nextInt();
                     sc.nextLine();
-                    Videojuego videojuego = videojuegos.get(id - 1);
+                    String anteriorNombre = videojuegos.get(id - 1).getNombre();
                     System.out.print("Nuevo nombre > ");
                     videojuegos.get(id - 1).setNombre(sc.nextLine());
-                    System.out.println("Nombre actualizado de " + videojuego.getNombre() + " a " + videojuegos.get(id - 1).getNombre());
+                    System.out.println("Nombre actualizado de " + anteriorNombre + " a " + videojuegos.get(id - 1).getNombre());
                     break;
                 
                 case 4:
+                    TiposDeTexto.H2("Eliminar videojuego", 8);
                     ListaElementos(videojuegos);
                     System.out.print("ID del videojuego a eliminar > ");
                     id = sc.nextInt();
@@ -66,6 +73,7 @@ public class Diego04 {
 
     private static int Menu() {
         int opcion;
+        TiposDeTexto.H1("Menu", 10);
         System.out.print("1.- Nuevo videojuego\n2.- Listar videojuegos\n3.- Actualizar nombre de videojuego\n4.- Eliminar videojuego\n5.- Salir\nOpcion > ");
         opcion = sc.nextInt();
         sc.nextLine();
@@ -73,7 +81,7 @@ public class Diego04 {
     }
 
     private static void ListaElementos(List<Videojuego> videojuegos) {
-        System.out.println(" ==== Elementos ====");
+        TiposDeTexto.H3("Elementos", 10);
         for (Videojuego elemento : videojuegos) {
             System.out.println(elemento.getId() + ".- " + elemento.getNombre());
         }
@@ -87,5 +95,37 @@ public class Diego04 {
     private static void LimpiarPantalla() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+    }
+
+    private class TiposDeTexto {
+        public static void H1(String texto, int espacios) {
+            System.out.println(
+                Colores.BLUE.TXT +
+                "=".repeat(espacios) +
+                " " + texto + " " +
+                "=".repeat(espacios) +
+                Colores.RESET
+            );
+        }
+
+        public static void H2(String texto, int espacios) {
+            System.out.println(
+                Colores.GREEN.TXT +
+                "=".repeat(espacios) +
+                " " + texto + " " +
+                "=".repeat(espacios) +
+                Colores.RESET
+            );
+        }
+
+        public static void H3(String texto, int espacios) {
+            System.out.println(
+                Colores.YELLOW.TXT +
+                "=".repeat(espacios) +
+                " " + texto + " " +
+                "=".repeat(espacios) +
+                Colores.RESET
+            );
+        }
     }
 }
